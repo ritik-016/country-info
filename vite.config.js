ef87a22ecc84c64272508642697bae995068cd71
Ritik Kumar <ad6360153@gmail.com>
2025-02-01 20:49:47 +0530

    Last commit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() ,tailwindcss()],
+  build: {rollupOptions: {
+    output: {
+      format: 'es',
+      entryFileNames: '[name].js',
+      chunkFileNames: '[name].js',
+      assetFileNames: '[name].[ext]'
+    }}
+  }
 })
